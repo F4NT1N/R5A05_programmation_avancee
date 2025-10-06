bc84c301bf2de9813b58d5992dff864d0d448515
F4NT1N <fantin.pesant@gmail.com>
2025-10-06 09:44:41 +0200

    init tp2

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.tp1.tp1;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Tp1ApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
